4b9506aeae4712578d516da6db14802fd9778400
Tai Hobbs <35228941+hobbstai@users.noreply.github.com>
2022-03-23 16:59:11 -0400

    Godot scene for all of the doors in the game

---
+++ Doors/Door.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+var can_click = false
+
+
+func _on_Door_body_entered(body):
+	if body.name == "Player":
+		can_click = true
+	else:
+		open()
+
+
+func _on_Door_body_exited(body):
+	if body.name == "Player":
+		can_click = false
+
+func _on_Door_input_event(viewport, event, shape_idx):
+	if Input.is_mouse_button_pressed(BUTTON_LEFT) and can_click:
+		open()
+
+func open():
+	$AnimationPlayer.play("Open")
+	
---
+++ Doors/LockedDoor.gd
@@ -0,0 +1,24 @@
+extends "res://Doors/Door.gd"
+
+func _ready():
+	$Label.rect_rotation = -rotation_degrees
+	
+
+func _on_Door_input_event(viewport, event, shape_idx):
+	if Input.is_mouse_button_pressed(BUTTON_LEFT) and can_click:
+		$CanvasLayer/Numpad.popup_centered()
+
+func _on_Door_body_exited(body):
+	if body.name == "Player":
+		can_click = false
+		$CanvasLayer/Numpad.hide()
+
+func _on_Numpad_combination_correct():
+	open()
+	$CanvasLayer/Numpad.hide()
+	
+
+func _on_Computer_combination(numbers, lock_group):
+	$CanvasLayer/Numpad.combination = numbers
+	$Label.text = lock_group
+
